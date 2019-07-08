0161188060f3bf67b45f250a7f1b1c985eaac8c0
carminenicoletta91 <carminenicolettakr91@gmail.com>
2019-07-08 21:42:22 +0200

    --terminato esercizio verificare se parola è palindroma--

--- Palindroma/pali.js
+++ Palindroma/pali.js
@@ -1,1 +1,25 @@
+// Faccio inserire una parola all'utente
+var scelta;
+scelta = prompt("Scegli una parola");
+console.log(scelta);
+// inverto la parola dell'utente
+var invertita;
+invertita = revString(scelta);
+console.log(invertita);
+// controllo se la parola è palindroma
+var esito;
+esito = palindroma(invertita , scelta);
+console.log(esito);
 
+// creo funzione per invertire una stringa
+function revString( str ) {
+    return str.split("").reverse().join("");
+}
+// creo funzione per comparare due stringhe
+function palindroma( x , y ){
+  if(  x === y ){
+    return "La prola è palindroma";
+  }else{
+    return "La parola non è palindroma";
+  }
+}
